f42a431daba803fe39a201a2a70e391b7671ce0a
Yancy <l_yancy@ats-hr420-dm19.local>
2019-11-12 14:43:10 -0500

    addingspiral

---
+++ assets/spiral.py
@@ -0,0 +1,22 @@
+from matplotlib import pyplot
+from turtle import *
+from random import randint
+
+speed(500000000000)
+bgcolor('black')
+
+x = 1
+
+while x < 400:
+	r = randint(0, 255)
+	g = randint(0, 255)
+	b = randint(0, 255)
+
+	colormode(255)
+
+	pencolor(r, g, b)
+
+	fd(200 + x)
+	rt(90.911)
+
+	x = x + 1
